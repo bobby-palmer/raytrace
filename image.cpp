2b53c75d5b999d5a7cc01f9dd82bc254160ff6ba
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-10-23 22:54:42 -0400

    Add color function

--- image.cpp
+++ image.cpp
@@ -22,6 +22,7 @@ color ray_color(const ray& r) {
   auto t = hit_sphere(point3(0,0,-1), 0.5, r);
   if (t > 0.0) {
     vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+    return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
   }
   vec3 unit_direction = unit_vector(r.direction());
   auto a = 0.5*(unit_direction.y() + 1.0);
