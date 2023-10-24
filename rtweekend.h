e596e8702ad5855c55b66d7f29d7339fbf03b8ef
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-10-24 10:45:39 -0400

    add random utilizty function

--- rtweekend.h
+++ rtweekend.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 //Using
 
@@ -19,6 +20,12 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+  static std::uniform_real_distribution<double> dist(0.0, 1.0);
+  static std::mt19937 gen;
+  return dist(gen);
 }
 
 // common headers
