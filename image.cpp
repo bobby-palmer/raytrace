1d1df9785d61243ce3d7f37ecab4f604c47df616
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-10-23 22:17:00 -0400

    used ray tracer for image generation

--- image.cpp
+++ image.cpp
@@ -2,12 +2,40 @@
 
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
+
+color ray_color(const ray& r) {
+  return color(0,0,0);
+}
 
 int main() {
   // Image
 
-  int image_width = 256;
-  int image_height = 256;
+  int image_width = 400;
+  auto aspect_ratio = 16.0 / 9.0;
+
+  // calc image height
+  int image_height = static_cast<int>(image_width / aspect_ratio);
+  image_height = (image_height < 1) ? 1 : image_height;
+
+  // Camera
+  auto focal_length = 1.0;
+  auto viewport_height = 2.0;
+  auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+  auto camera_center = point3(0,0,0);
+
+  // Calculate the vectors across the horizontal and down the vertical viewport edges.
+  auto viewport_u = vec3(viewport_width, 0, 0);
+  auto viewport_v = vec3(0, -viewport_height, 0);
+
+  // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+  auto pixel_delta_u = viewport_u / image_width;
+  auto pixel_delta_v = viewport_v / image_height;
+
+  // Calculate the location of the upper left pixel.
+  auto viewport_upper_left = camera_center
+                           - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+  auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
   // Render
 
@@ -16,8 +44,12 @@ int main() {
   for (int i = 0; i < image_height; ++i) {
     for (int j = 0; j < image_width; ++j) {
       std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-      auto pixel = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
-      write_color(std::cout, pixel);
+      auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+      auto ray_direction = pixel_center - camera_center;
+      ray r(camera_center, ray_direction);
+
+      color pixel_color = ray_color(r);
+      write_color(std::cout, pixel_color);
     } // for col
   } // for row
   std::clog << "\rDone\n";
