c1bfbcd969f893584b69b21dc7fddf5a5070df4c
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-11-08 18:11:53 -0500

    added fuzzyness

--- material.h
+++ material.h
@@ -35,14 +35,15 @@ private:
 
 class metal : public material {
 public:
-  metal(const color& a) : albedo(a){}
+  metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
   virtual bool scatter(const ray& ray_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
     vec3 reflected = reflect(unit_vector(ray_in.direction()), rec.normal);
-    scattered = ray(rec.p, reflected);
+    scattered = ray(rec.p, reflected + fuzz*random_unit_vector());
     attenuation = albedo;
-    return true;
+    return (dot(scattered.direction(), rec.normal) > 0);
   }
 private:
   color albedo;
+  double fuzz;
 };
