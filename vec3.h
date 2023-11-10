56c33f0f932f92a7056876f92cec8f8f9bc29644
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-11-09 21:57:27 -0500

    fix bug in multiplication of colors

--- vec3.h
+++ vec3.h
@@ -77,7 +77,7 @@ inline vec3 operator-(const vec3& u, const vec3& v) {
 }
 
 inline vec3 operator*(const vec3& u, const vec3& v) {
-  return vec3(u[0] * v[0], u[1]* v[1], u[2] * u[2]);
+  return vec3(u[0] * v[0], u[1]* v[1], u[2] * v[2]);
 }
 
 inline vec3 operator*(double t, const vec3& v) {
